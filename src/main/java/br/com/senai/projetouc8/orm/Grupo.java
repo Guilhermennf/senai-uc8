a49433a49e91bd4bf47ee274d7dd3ec125751053
Guilherme <guilhermenunesbh5@gmail.com>
2022-08-01 08:49:56 -0300

    Versão 0.0.2 - Projeto UC8

--- src/main/java/br/com/senai/projetouc8/orm/Grupo.java
+++ src/main/java/br/com/senai/projetouc8/orm/Grupo.java
@@ -4,12 +4,16 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name="grupos")
 public class Grupo {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private	Integer id;
+	private Integer id;
+	private String grupo;
+	
 	public Integer getId() {
 		return id;
 	}
@@ -22,5 +26,4 @@ public class Grupo {
 	public void setGrupo(String grupo) {
 		this.grupo = grupo;
 	}
-	private String grupo;
 }
